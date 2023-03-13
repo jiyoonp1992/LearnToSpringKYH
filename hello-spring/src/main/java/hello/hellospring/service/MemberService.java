b4a5de247a1d658ffd2191c9514dc57e2e5f3c28
jiyoonpark <jiyoonp1992@gmail.com>
2023-03-13 17:39:44 +0900

    0313

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,12 @@ import hello.hellospring.repository.MemoryMemberRepository;
 
 public class MemberService {
 	
-	private final MemberRepository memberRepository = new MemoryMemberRepository();
+	private final MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+		
+	}
 	
 	/*
 	 * 회원가입
